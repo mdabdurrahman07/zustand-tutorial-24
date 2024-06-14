ce66d34319c1c5bb7c1d42eae8654986e8b6a4c9
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-06-14 19:38:07 +0600

    product card added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import { useStore } from "./store/store";
+import { ProductData } from "@/lib/mockData.ts";
+import { Card, CardContent, CardFooter, CardHeader } from "./components/ui/card";
+import { Button } from "./components/ui/button";
+import { useStore } from "@/store/store";
+
 
 const App = () => {
 
-  const store = useStore()
+  const addProduct = useStore((state) => state.addProduct)
+  const products = useStore((state) => state.Products)
+  
   return (
-    <div>
-      {JSON.stringify(store)}
+    <div className="space-y-2 h-screen max-w-sm mx-auto dark bg-background mt-2">
+      <h1 className="text-2xl">Products:</h1>
+      <div className="space-y-2">
+      {ProductData.map((products) => (
+        <Card key={products.id}>
+          <CardHeader>
+            {products.title}
+          </CardHeader>
+          <CardContent>
+            ${products.price}
+          </CardContent>
+          <CardFooter>
+            <Button onClick={() => addProduct(products)} variant="default">
+              Add to cart
+            </Button>
+          </CardFooter>
+        </Card>
+      ))}
+      </div>
+      
     </div>
   );
 };
